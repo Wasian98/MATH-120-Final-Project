41dcc67bd9f9334a968fc5c20d5d92291d3caae0
Zacheary Vang <132774505+Wasian98@users.noreply.github.com>
2025-12-19 15:32:19 -0800

    Implement data loading, cleaning, and saving functions

---
+++ src/data_processing.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+def load_raw_data(file_path):
+    """Load raw data from CSV file."""
+    return pd.read_csv(file_path)
+
+def clean_data(df):
+    """Cleans up and keeps specific columns"""
+    cols_to_keep = ["name", "release_date", "price", "metacritic_score", "genres", "user_score", "estimated_owners", "average_playtime_forever", "average_playtime_2weeks", "num_reviews_total"]
+    df = df[cols_to_keep]
+    return df
+
+def save_cleaned_data(df, output_path):
+    """Save cleaned data to CSV file."""
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    df.to_csv(output_path, index=False)
